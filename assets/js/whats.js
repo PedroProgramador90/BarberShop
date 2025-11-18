2cb7c589d93c6ac2d24bab2bad4618f3c12da27a
Pedro Henrique Dias de Souza <pedrohenrique5590@hotmail.com>
2025-11-18 15:57:17 -0300

    Add files via upload

---
+++ assets/js/whats.js
@@ -0,0 +1,14 @@
+function enviarWhats(event) {
+const nome = document.getElementById('nome').value;
+const mensagem = document.getElementById('mensagem').value;
+const telefone = '5524999999999'
+
+const texto = `Olá! Me chamo ${nome}, ${mensagem}`
+const msgFormatada = encodeURIComponent(texto)
+
+const url = `https://wa.me/${telefone}?text=${msgFormatada}`
+
+console.log(url)
+
+window.open(url,'_blank')
+}
